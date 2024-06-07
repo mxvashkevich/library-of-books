893b08b07ec1726a1bab33d4f7b354a6cb4e6359
vashkevichma <vashkevich@softlab.ru>
2024-06-07 04:25:11 +0300

    feat(main): main in progress

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,3 +1,21 @@
+import { BOOK_STORE } from "./constants"
+
+import "./styles.scss";
+
 export const Main = () => {
-  return <div>ХУЙХУЙХУЙ</div>
+  return <div className="container">
+    {BOOK_STORE.map((book) => (
+      <div className="book-item">
+        <div className="book-img">
+          <img src={book.image} alt={`book-image-named: ${book.title}`} className="book-img" />
+        </div>
+        <div className="book-info">
+          <h3>{book.title}</h3>
+          <p>{book.description}</p>
+          <p>{book.author}</p>
+          <p>Цена: {book.countStorage}</p>
+        </div>
+      </div>
+    ))}
+  </div>
 }
